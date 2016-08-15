bbf9fa25674546e68293a823326369ad6cbbc67c
Ciaran Hale <ciaranhale@gmail.com>
2016-08-15 14:33:19 +1000

    Add Better Chat

--- chat-voda/javascripts/client/PraiseBlogClient.js
+++ chat-voda/javascripts/client/PraiseBlogClient.js
@@ -28,14 +28,30 @@ PraisePostService = {
 Template.praiseBlog.events({
 	'submit .newPraisePostForm': function(event) {
 		// This function is called when the new task form is submitted
-		var text = event.target.text.value;
+		if (event.target.text.value.length > 1) {
+			var text = event.target.text.value;
 
-		Meteor.call('addPost', text);
+			Meteor.call('addPost', text);
 
-		// Clear form
-		event.target.text.value = '';
-		// Prevent default form submit
+			// Clear form
+			event.target.text.value = '';
+			// Prevent default form submit
+		}
 		return false;
+	},
+	'keyup .addNewPraisePost': function(event) {
+		if (event.which === 13) {
+			// This function is called when the new task form is submitted
+			var text = event.target.value;
+			if (event.target.value.length > 1) {
+				Meteor.call('addPost', text);
+				// Clear form
+				event.target.value = '';
+				// Prevent default form submit
+			}
+			return false;
+
+		}
 	}
 });
 
